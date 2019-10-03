7be3bc20b38d1b565dbd3d4fba2eefd5199d7c8e
GogusonPL <yasni.exe@gmail.com>
2019-10-03 11:10:18 +0200

    Removed MainWindow.xaml and added ImgFilterWindow.xaml

---
+++ ImgFilters/View/ImgFiltersWindow.xaml.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace ImgFilters.View
+{
+    /// <summary>
+    /// Interaction logic for ImgFiltersWindow.xaml
+    /// </summary>
+    public partial class ImgFiltersWindow : Window
+    {
+        public ImgFiltersWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
